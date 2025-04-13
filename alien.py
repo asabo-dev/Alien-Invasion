7e2cfe6482da8a810c072634a78e5eb95a05a7a4
Efiom Ndaeyo <rylshadow@yahoo.com>
2025-04-13 19:22:07 +0800

    Create an Instance of the Alien.

--- alien.py
+++ alien.py
@@ -14,8 +14,8 @@ class Alien(Sprite):
         self.rect = self.image.get_rect()
 
         # Start each new alien near the top left of the screen.
-        self.rect.x = self.rect.get_width
-        self.rect.y = self.rect.get_height
+        self.rect.x = self.rect.width
+        self.rect.y = self.rect.height
 
         # Store the alien's exact horizontal position.
         self.x = float(self.rect.x)
